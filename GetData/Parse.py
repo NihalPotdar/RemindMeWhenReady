828159836cebd270a9d374879a0881e2e00169c1
Nihal Potdar <Nihalpot2002@gmail.com>
2020-02-27 23:07:33 -0700

    added basic bot

--- GetData/Parse.py
+++ GetData/Parse.py
@@ -1,3 +1,5 @@
+# the aim of this app is to act as a social media handler
+'''
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,4 +12,23 @@ soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find('')
 # Printing out the content for testing
 print(results.prettify())
+'''
 
+import tweepy
+import credentials
+
+auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret_key)
+auth.set_access_token(credentials.access_key, credentials.access_key_secret)
+api = tweepy.API(auth)
+
+'''
+# simple means for printing out whatever is on the home console
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet)
+'''
+
+# user object for twitter
+user = api.get_user('NihalPotdar')
+print(user.screen_name)
+print(user.followers_count)
