db32d51de1ef763716e15a65de74484cf1c1e019
Nihal Potdar <Nihalpot2002@gmail.com>
2020-02-28 23:08:31 -0700

    working obj feature

--- GetData/Parse.py
+++ GetData/Parse.py
@@ -24,62 +24,53 @@ auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret
 auth.set_access_token(credentials.access_key, credentials.access_key_secret)
 api = tweepy.API(auth)
 
-# getting tweets on home_timeline
-'''
-# simple means for printing out whatever is on the home console
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet)
 '''
 
 # connect to mongo_db
 client = MongoClient(credentials.mongo_db_client)
 
 #finding where there is a mention of name
-#print("\n'''''''''''RECENT MENTIONS''''''''''''''''''''''")
-
-# recent mentions on twitter
-recent_mentions = api.mentions_timeline()
 
 # make a post to mongo_db, take mentions and add to db
 mentions_collection = client.Twitter_API.Mentions 
 
 # remove everything in the database
-mentions_collection.delete_many({})
-
-# add a document to the database
-for mention in recent_mentions:
-    
-    '''
-    mentions_collection.insert_one(
-        mention._json
-    )
-    '''
-
-try:
-    print(mention._json['entities']['urls'][0]['expanded_url'])
-except:
-    print("There's an error!")
-    
-    print(mention.user.name)
-    print(mention.user.screen_name)
-    print(mention.text)
-    print(mention.retweet_count)
-    print(mention.favorite_count)
-
-#print("'''''''''''''''''''''''''''''''''''''''''''''''''")
+#mentions_collection.delete_many({})
 
 # finding if a required friend has made a post
 required_friends = ['YouthCentralYYC']
 
 
-#print("\n''''''''''''''''FRIENDS'''''''''''''''''''''''''")
-# user object for twitter
+def friends():
+    # user object for twitte
+    user = api.get_user('NihalPotdar')
+    print(user.screen_name)
+    print(user.followers_count)
+    for friend in user.friends():
+        print(friend.screen_name)
 '''
-user = api.get_user('NihalPotdar')
-print(user.screen_name)
-print(user.followers_count)
-for friend in user.friends():
-    print(friend.screen_name)
+
+def upload(mention):
+    upload = {}
+    try:
+        upload['url'] = mention._json['entities']['urls'][0]['expanded_url']     
+        upload['name'] = mention.user.name
+        upload['twitter_handle'] = mention.user.screen_name
+        upload['tweet'] = mention.text
+        upload['retweet_count'] = mention.retweet_count
+        upload['liked count'] = mention.favorite_count
+        print(upload)
+    except:
+        print("There's an error!")
+
 '''
-#print("'''''''''''''''''''''''''''''''''''''''''''''''''")
+        mentions_collection.insert_one(
+            mention._json
+        )
+'''
+
+# recent mentions on twitter
+recent_mentions = api.mentions_timeline()
+# add a document to the database
+for mention in recent_mentions:
+    upload(mention)
