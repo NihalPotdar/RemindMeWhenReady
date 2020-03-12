fe84facca7df62c935868d15ea8bf27af99423fa
Nihal Potdar <Nihalpot2002@gmail.com>
2020-03-11 22:56:27 -0400

    big fixes

--- SendFiles/CheckForUpdates.py
+++ SendFiles/CheckForUpdates.py
@@ -1,18 +1,23 @@
 # write algorithm which takes a list of names of some-kind and sends updates to an http endpoint at some interval
 from pymongo import MongoClient
 import credentials
+import requests
 
 Warehouse = MongoClient(credentials.mongo_db_client) 
+list_db = []
+test_endpoint = "https://eng3ecop7bsoc.x.pipedream.net"
 
 def checkforupdate():
-    # get list of databases
-
     # find a list of databases
     for i in Warehouse.list_databases({}):
         name = i['name']
         if name != "admin" and name != 'local':
-            print(name)
+            list_db.append(i['name'])
 
     # compare list of databases and based on some requirement, send http request to specified endpoint
 
+def send_post():
+    print(requests.get(test_endpoint))
+
 checkforupdate()
+send_post()
