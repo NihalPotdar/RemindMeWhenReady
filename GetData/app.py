12ae98f0f9656de470211ed443bccfae6732bffa
Nihal Potdar <Nihalpot2002@gmail.com>
2020-03-15 18:13:57 -0400

    added flask requirement

--- GetData/app.py
+++ GetData/app.py
@@ -10,5 +10,5 @@ def interact():
     ParseTwitter.console()
     return "Successfully called"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')    
