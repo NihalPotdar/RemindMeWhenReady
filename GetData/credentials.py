a49302a217d300db373f2c61c3616ebe50532cc9
Nihal Potdar <Nihalpot2002@gmail.com>
2020-03-15 16:58:49 -0400

    added credentials

---
+++ GetData/credentials.py
@@ -0,0 +1,5 @@
+consumer_key = "iPMgSmFwuAn66ILhtnI9jN87h" # consumer API key
+consumer_secret_key = "HvsL6iqqDFdtzrpqWe7oTR5erMsTkExnycwLLcB8N8GXjtBHTC" #consumer secret key
+access_key = "1046221435289497601-W9E5XjjwfQuGteAhSIFwFVy1FqKJM4" #the access key
+access_key_secret = "jIi9Wlvug9jOdGuj70caVHpjHGwDrXbKjDhTgdsv7xF0H" #the secret access key
+mongo_db_client = "mongodb+srv://dbUser:ConnectTwitter22@cluster0-c7ut5.mongodb.net/test?retryWrites=true&w=majority" #mongo db connection key
